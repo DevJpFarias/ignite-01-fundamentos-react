87fcd22767de2210cfbfe9f3671da378625a5de2
João <joaopfc2764@gmail.com>
2023-03-31 21:05:13 -0300

    insert Header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="João Paulo" content="Alguma coisa"/>
       <Post author="Ana Paula" content="Outra coisa"/>
     </div>
