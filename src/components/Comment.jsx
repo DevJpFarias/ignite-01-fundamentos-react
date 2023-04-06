6b7b1231819fb5bdea2592723851c6a99279ccb9
João <joaopfc2764@gmail.com>
2023-04-06 19:55:24 -0300

    create Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import styles from './Comment.module.css'
+import { Trash, ThumbsUp } from 'phosphor-react'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/75543735?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João Paulo</strong>
+              <time title='11 de Maio de 2022 às 08:13h' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div> 
+  )
+}
